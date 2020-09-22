1ddeb986937fe0e96226a752aad44225d6a45a23
kayla <kkingston09@gmail.com>
2020-09-22 13:07:26 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newTaskForm = document.getElementById('create-task-form');
+  const newTaskDescription = document.getElementById('new-task-description');
+  const newTaskPriority = document.getElementById('new-task-priority');
+
+  const newTaskUl = document.getElementById('tasks');
+  
+  //Event listeners here
+  newTaskForm.addEventListener('submit', createNewTask);
+  
 });
+
+const createNewTask = event => {
+  event.preventDefault();
+
+  const newTaskDescription = document.getElementById('new-task-description');
+  const newTask = document.createElement('li');
+  newTask.innerText = newTaskDescription.value;
+  
+  appendNewTask(newTask);
+  event.target.reset();
+};
+
+const appendNewTask = task => {
+  document.getElementById('tasks').appendChild(task);
+};
+
+
+//grab text from text bar
+
+//event listener for submit button //prevent redirect
+
+//put text in todo list
+
